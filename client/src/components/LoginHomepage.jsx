5154cd3303877f7513745153659d5656ff2ba773
Angela Lim <angela.lim57@myhunter.cuny.edu>
2020-10-03 20:58:06 -0400

    adding cards for prompts

--- client/src/components/LoginHomepage.jsx
+++ client/src/components/LoginHomepage.jsx
@@ -9,7 +9,21 @@ class LoginHomepage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        date: new Date()
+        date: new Date(),
+        prompts: [
+            {
+                prompt: "Write 3 things you're grateful for"
+            },
+            {
+                prompt: "Write about your day"
+            },
+            {
+                prompt: "What are you anxious for"
+            },
+            {
+                prompt: "Who are you grateful for"
+            }
+        ]
     }
     this.onChange = this.onChange.bind(this);
   }
@@ -19,10 +33,12 @@ class LoginHomepage extends Component {
           date: date
       })
   }
+
+
   render() {
     return (
       <div>
-          <Navbar>
+          <Navbar className="underline">
                 <Navbar.Brand href="/">
                     <img src={ZenyuLogo} 
                         width="30" 
@@ -31,11 +47,33 @@ class LoginHomepage extends Component {
                         alt="Zenyu Logo">
                     </img>
                 </Navbar.Brand>
+                <Navbar.Collapse className = "justify-content-end">
+                    <Navbar.Text>
+                        Welcome User
+                    </Navbar.Text>
+                </Navbar.Collapse>
           </Navbar>
-          <Calendar
-            onChange={this.onChange}
-            value={this.state.date}
-          />
+          <div className = "homepage-layout">
+            <Calendar
+                onChange={this.onChange}
+                value={this.state.date}
+            />
+            <div className = "prompt-layout">
+                {this.state.prompts.map((prompt,index)=>(
+                    <Card style={{width: '18rem'}}>
+                    <Card.Header>Today's Prompt</Card.Header>
+
+                    <Card.Body>
+                        <Card.Text>{prompt.prompt}</Card.Text>
+                        <Card.Link href="/">Select This Prompt</Card.Link>
+                    </Card.Body>
+                </Card>
+                ))}
+                
+            </div>
+            
+          </div>
+          
       </div>
     );
   }
