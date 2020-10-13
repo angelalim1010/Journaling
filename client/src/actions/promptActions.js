9779e428797e2eb5604ee8e90e05bd141276fb1a
Angela Lim <angela.lim57@myhunter.cuny.edu>
2020-10-13 12:58:35 -0400

    adding proxy to package lock + fixing sme css

--- client/src/actions/promptActions.js
+++ client/src/actions/promptActions.js
@@ -2,4 +2,10 @@ import axios from "axios";
 
 export const getPrompts = async ()=>{
     console.log("getting prompts")
+    try{
+        return await axios.get('/api/journalprompts')
+    }
+    catch(e){
+        console.log(e.response)
+    }
 }
