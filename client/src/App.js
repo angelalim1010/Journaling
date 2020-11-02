571494fe7cacb08043be61d36da5a0eae0f7ad0e
Gillian Pantaleon <gillybytes@gmail.com>
2020-11-02 15:16:45 -0500

    added authstate

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,60 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 import LoginHomepage from './components/LoginHomepage';
 import MainHomepage from './components/MainHomepage';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
+import AuthState from './context/auth/authState';
+
+
+const App = () => {
   return (
-    <Router>
-      <div className='App'>
-        <div className='auth-wrapper'>
-          <div className='auth-inner'>
-            <Switch>
-              <Route exact path='/' component={Login} />
-              <Route path='/login' component={Login} />
-              <Route path='/signup' component={Signup} />
-              <Route path='/homepage' component={LoginHomepage} />
-              <Route path= '/mainhomepage' component={MainHomepage} />
-            </Switch>
+    <AuthState>
+      <Router>
+        <div className='App'>
+          <div className='auth-wrapper'>
+            <div className='auth-inner'>
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={Signup} />
+                <Route path='/homepage' component={LoginHomepage} />
+                <Route path= '/mainhomepage' component={MainHomepage} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthState>
   );
 }
+
+// function App() {
+//   return (
+//     <AuthState>
+//       <Router>
+//         <div className='App'>
+//           <div className='auth-wrapper'>
+//             <div className='auth-inner'>
+//               <Switch>
+//                 <Route exact path='/' component={Login} />
+//                 <Route path='/login' component={Login} />
+//                 <Route path='/signup' component={Signup} />
+//                 <Route path='/homepage' component={LoginHomepage} />
+//                 <Route path= '/mainhomepage' component={MainHomepage} />
+//               </Switch>
+//             </div>
+//           </div>
+//         </div>
+//       </Router>
+//     </AuthState>
+//   );
+// }
 
 export default App;
