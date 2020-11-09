36dc5c9f2df0bd51af1d1d01e130982262736197
skabir8 <shariar.k8@gmail.com>
2020-11-09 12:40:27 -0500

    fix auth setting error

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 const setAuthToken = token => {
     if (token) {
         axios.defaults.headers.common['Authorization'] = token;
+        
     } else {
         delete axios.defaults.headers.common['Authorization'];
     }
+    console.log(axios.defaults.headers.common);
 };
 
 export default setAuthToken;
