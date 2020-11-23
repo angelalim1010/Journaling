71f8cd3ab188c9ab1602837a2e6e0bb3672ba451
Gillian Pantaleon <gillybytes@gmail.com>
2020-11-22 20:46:10 -0500

    changed all components in landing to stateless functional components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Nav } from 'react-bootstrap';
 
 
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-          <Container id="footer" fluid>
-            <Nav className="justify-content-center" activeKey="/home">
-              <Nav.Item>
-                <Nav.Link className="text" eventKey="disabled" disabled>
-                    Copyright © 2020 Zenyu. All rights reserved.
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#" className="text" eventKey="contact">Contact</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="terms" className="text" eventKey="tos">Terms of Service</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="disclaimer" className="text" eventKey="disclaimer">Disclaimer</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="privacy" className="text" eventKey="privacy">Privacy Policy</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Container>
-      </div>
-    )
-  }
+const Footer = () => {
+  return (
+    <div>
+        <Container id="footer" fluid>
+          <Nav className="justify-content-center" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link className="text" eventKey="disabled" disabled>
+                  Copyright © 2020 Zenyu. All rights reserved.
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" className="text" eventKey="contact">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="terms" className="text" eventKey="tos">Terms of Service</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="disclaimer" className="text" eventKey="disclaimer">Disclaimer</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="privacy" className="text" eventKey="privacy">Privacy Policy</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+    </div>
+  )
 }
 
 export default Footer;
